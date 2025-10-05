6a370b53de6189265eed1d97241f7b8ec8a3222e
Karol Moroz <karol@moroz.dev>
2025-10-05 14:09:23 +0800

    Add seeds

--- MyApp/Migrations/Configuration.cs
+++ MyApp/Migrations/Configuration.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Common;
+﻿using System;
+using System.Data.Common;
 using System.Data.Entity;
 using System.Data.Entity.Migrations.History;
 using MyApp.Models;
@@ -18,7 +19,27 @@ namespace MyApp.Migrations
 
         protected override void Seed(AppDbContext context)
         {
-            // Seed data comes here
+            context.Products.AddOrUpdate(new Product[]
+            {
+                new Product
+                {
+                    ProductId = new Guid("0199b327-17b7-7e22-8926-c35f6d19a1e8"),
+                    Title = "Wrangler GREENSBORO Sunset Rinse W34 L32",
+                    SKU = "112357414 W34 L32",
+                    Price = 56.29M,
+                    Description = "Wrangler Greensboro 803 Regular Straight Sunset Rinse W34 L32",
+                    ImageUrl = "https://a.allegroimg.com/original/111327/73f12ec0425ab72d4cb0821314e1",
+                },
+                new Product
+                {
+                    ProductId = new Guid("0199b32b-32ef-7676-804f-f2206b8c2d92"),
+                    Title = "Wrangler LARSTON Rodeo FREE TO STRETCH W32 L32",
+                    SKU = "5401139119734 W32 L32",
+                    Description = "Wrangler Larston 812 Slim Tapered FREE TO STRETCH Midnight Rodeo W32 L32",
+                    Price = 47.05M,
+                    ImageUrl = "https://a.allegroimg.com/original/114ba9/1f6db55d4a8faf676810614e2c0b",
+                }
+            });
         }
     }
 
